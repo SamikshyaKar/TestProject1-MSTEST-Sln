2e1b889c37292417c6c0096f38710b05b51af732
samikshya.sibani <samikshya.sibani@gmail.com>
2022-01-22 19:19:27 +0530

    -- next commit

--- TestProject1(MSTEST)-1/UnitTest1.cs
+++ TestProject1(MSTEST)-1/UnitTest1.cs
@@ -12,5 +12,21 @@ namespace TestProject1_MSTEST__1
             p = 15;
             Assert.IsFalse(p < 15, "Test passed");
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int p = 10;
+            p = 15;
+            Assert.IsTrue(p == 15, "Test passed");
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int p = 10;
+            p = 15;
+            Assert.IsNotNull(p == 15, "Test passed");
+        }
     }
 }
